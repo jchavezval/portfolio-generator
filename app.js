d8ba4364e5ac45b4fe0e3519819815122b33bb52
Juan <jmchv2001@gmail.com>
2021-02-10 18:19:38 -0800

    readme generated with node.js

---
+++ app.js
@@ -0,0 +1,27 @@
+// const animalArray = ['dog', 'cat', 'pig'];
+
+// animalArray.push('cow');
+
+// const personObj = {
+//   name: 'Lernantino',
+//   age: 99
+// };
+
+// personObj.age = 100;
+// personObj.occupation = 'Developer';
+
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
