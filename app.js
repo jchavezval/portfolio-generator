d45a35b45df21025cc6249032c75e83d93a74368
Juan <jmchv2001@gmail.com>
2021-02-12 14:05:47 -0800

    prompt.inquirer

--- app.js
+++ app.js
@@ -1,27 +1,34 @@
-// const animalArray = ['dog', 'cat', 'pig'];
+//To add element to an array:
+  // const animalArray = ['dog', 'cat', 'pig'];
+  // animalArray.push('cow');
 
-// animalArray.push('cow');
+  // const personObj = {
+  //   name: 'Lernantino',
+  //   age: 99
+  // };
+  // personObj.age = 100;
+  // personObj.occupation = 'Developer';
 
-// const personObj = {
-//   name: 'Lernantino',
-//   age: 99
-// };
+const inquirer = require('inquirer'); 
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-// personObj.age = 100;
-// personObj.occupation = 'Developer';
+//   //const profileDataArgs = process.argv.slice(2);
+//   //const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
-const fs = require('fs');
+// fs.writeFile('./index.html', pageHTML(name, github), err => {
+//   if (err) throw err;
 
-const generatePage = require('./src/page-template.js');
-
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
-
-
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ]);
+};
+promptUser().then(answers => console.log(answers));
